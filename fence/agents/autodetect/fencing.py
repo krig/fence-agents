a27f01f835de5abc9b1f362b88a56072525872d7
Kristoffer Grönlund <krig@koru.se>
2017-11-01 10:28:22 +0100

    Use Python 3 for all scripts (bsc#1065966)

--- fence/agents/autodetect/fencing.py
+++ fence/agents/autodetect/fencing.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -tt
+#!/usr/bin/python3 -tt
 
 import sys, getopt, time, os, uuid, pycurl, stat
 import pexpect, re, syslog
--- tests/test-drac4.py
+++ tests/test-drac4.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from fence_testing import test_action
 
